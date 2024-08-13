cc0d9ed79f60f8220ba9862034316597d368e529
huseyineskan01@gmail.com <huseyineskan01@gmail.com>
2024-08-13 23:06:08 +0200

    added js

--- script.js
+++ script.js
@@ -46,9 +46,53 @@ class Player {
       this.velocity.y = 0;
     }
 
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
 
-
-
+    if (this.position.x >= canvas.width - this.width * 2) {
+      this.position.x = canvas.width - this.width * 2;
+    }
   }
+}
+
+const player = new Player();
+
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+
+  if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
+    player.velocity.x = 5;
+  } else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
+    player.velocity.x = -5;
+  } else {
+    player.velocity.x = 0;
+  }
+}
+
+
+const keys = {
+  rightKey: {
+    pressed: false
+  },
+  leftKey: {
+    pressed: false
+  }
+};
+
+
+const movePlayer = (key, xVelocity, isPressed) => {
 
 }
+
+
+const startGame = () => {
+  canvas.style.display = "block";
+  startScreen.style.display = "none";
+  player.draw();
+}
+
+startBtn.addEventListener("click", startGame);
